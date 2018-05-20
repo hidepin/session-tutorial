d88245e21c96f9a628d50cf64bff8d5b31f95a15
Hidetoshi Imai <hidepin@gmail.com>
2018-05-21 00:30:04 +0900

    Add rest if.

---
+++ session-tutorial-init-web/src/main/java/com/example/session/api/account/AccountResource.java
@@ -0,0 +1,22 @@
+package com.example.session.api.account;
+
+import java.io.Serializable;
+
+public class AccountResource implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
---
+++ session-tutorial-init-web/src/main/java/com/example/session/api/account/AccountRestController.java
@@ -0,0 +1,28 @@
+package com.example.session.api.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.session.domain.model.Account;
+
+@RestController
+@RequestMapping("account")
+public class AccountRestController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public List<AccountResource> getAccount() {
+		List<AccountResource> accountResources = new ArrayList<>();
+		AccountResource accountResource = new AccountResource();
+		accountResource.setUsername("sample");
+		accountResources.add(accountResource);
+		return accountResources;
+	}
+	
+}
