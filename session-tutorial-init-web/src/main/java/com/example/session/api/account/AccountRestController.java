e8963760a849424bfec08bcaab4f1a7f3bd4457a
Hidetoshi Imai <hidepin@gmail.com>
2018-05-21 22:08:36 +0900

    Add sessionregistry.

--- session-tutorial-init-web/src/main/java/com/example/session/api/account/AccountRestController.java
+++ session-tutorial-init-web/src/main/java/com/example/session/api/account/AccountRestController.java
@@ -3,7 +3,10 @@ package com.example.session.api.account;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("account")
 public class AccountRestController {
 
+	@Inject
+	SessionRegistry sessionRegistry;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public List<AccountResource> getAccount() {
 		List<AccountResource> accountResources = new ArrayList<>();
 		AccountResource accountResource = new AccountResource();
-		accountResource.setUsername("sample");
+		accountResource.setUsername(sessionRegistry.toString());
 		accountResources.add(accountResource);
 		return accountResources;
 	}
