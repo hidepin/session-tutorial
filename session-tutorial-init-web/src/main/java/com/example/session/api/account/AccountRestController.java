7b21c523f4e83da140e5d110a00666be868db92c
Hidetoshi Imai <hidepin@gmail.com>
2018-05-21 22:52:29 +0900

    add impl.

--- session-tutorial-init-web/src/main/java/com/example/session/api/account/AccountRestController.java
+++ session-tutorial-init-web/src/main/java/com/example/session/api/account/AccountRestController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountRestController {
 
 	@Inject
-	SessionRegistry sessionRegistry;
+	SessionRegistryImpl sessionRegistry;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
